bdad925f7735d53ee63522c3576dbc2bbdc66b19
Håkan Edling <hakan@tidyui.com>
2016-02-24 14:09:00 +0100

    Added mixed mode example

---
+++ MixedMode/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace MixedMode.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index() {
+            return View();
+        }
+
+		public ActionResult SubPage() {
+			return View();
+		}
+    }
+}
