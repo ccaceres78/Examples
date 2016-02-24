eda445be1fe7ea3a11a24d0c9a8f6659e7623dd9
Håkan Edling <hakan@tidyui.com>
2016-02-24 14:18:54 +0100

    Added post controller

---
+++ MixedMode/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using Piranha.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MixedMode.Controllers
+{
+    public class PostController : SinglePostController
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+			var model = GetModel();
+
+            return View(model.GetView(), model);
+        }
+    }
+}
